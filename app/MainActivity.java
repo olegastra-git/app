543b851b4db47fac9d50581fee1feca4b18dc406
olegastra-git <112951133+olegastra-git@users.noreply.github.com>
2023-10-14 12:00:43 +0300

    app

--- app/MainActivity.java
+++ app/MainActivity.java
@@ -1,3 +1,16 @@
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private MessageAdapter messageAdapter;
